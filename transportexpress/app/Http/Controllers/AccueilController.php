13eac3c035ac6a4a5428d202013da47ebae62c83
Hounaida202 <etrichih@gmail.com>
2025-04-11 16:15:59 +0000

    la methode show dans le controller pour l'affiche de blade de l'accueil

--- transportexpress/app/Http/Controllers/AccueilController.php
+++ transportexpress/app/Http/Controllers/AccueilController.php
@@ -9,13 +9,5 @@ class AccueilController extends Controller
     public function show(){
         return  view('Accueil'); 
     }
-    public function inscription(){
-        return  view('SignUp'); 
-    }
-    public function connexion(){
-        return  view('LogIn'); 
-    }
-    public function index(){
-        return  view('Dashbaord'); 
-    }
+    
 }
