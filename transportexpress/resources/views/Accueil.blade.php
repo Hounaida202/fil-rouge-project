828a5ffd06c5e972efdf3f4452b1c546a1de8acf
Hounaida202 <etrichih@gmail.com>
2025-04-12 23:01:38 +0000

    un div pour les 3 services

--- transportexpress/resources/views/Accueil.blade.php
+++ transportexpress/resources/views/Accueil.blade.php
@@ -41,7 +41,8 @@
     <section class="py-12 bg-white">
         <div class="container mx-auto px-4">
         <h2 class="text-3xl font-bold text-center mb-8">Nos Services</h2>
-            
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+               
         </div>
     </section>
     <!-- ------------------------------------------------ -->
