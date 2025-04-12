7b9ebe136966b8449c82b4afb068590b8055636b
Hounaida202 <etrichih@gmail.com>
2025-04-12 23:45:17 +0000

    ajouter la section de la formulaire

--- transportexpress/resources/views/SignUp.blade.php
+++ transportexpress/resources/views/SignUp.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<body>
-    
+<body class="bg-gray-100 font-sans">
+<section class="py-12">
+        
+    </section>
 </body>
 </html>
