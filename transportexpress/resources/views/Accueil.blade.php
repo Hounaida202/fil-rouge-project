16fdfd314f10202866411dc8524be5e7233dac6c
Hounaida202 <etrichih@gmail.com>
2025-04-12 22:19:51 +0000

    le div de navbar avec son style en css tailwind

---
+++ transportexpress/resources/views/Accueil.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="bg-gray-100 font-sans">
+    <nav class="bg-blue-600 text-white p-4 shadow-md">     
+    </nav>
+
+  
+
+
+
+
+
+   
+
+   
+</body>
+</html>
