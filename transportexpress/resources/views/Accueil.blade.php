9387d1565cb963304a3319cc90e682811036a46e
Hounaida202 <etrichih@gmail.com>
2025-04-12 22:39:08 +0000

    les composants de navbar avec le style compatible

--- transportexpress/resources/views/Accueil.blade.php
+++ transportexpress/resources/views/Accueil.blade.php
@@ -8,11 +8,16 @@
 <body class="bg-gray-100 font-sans">
     <nav class="bg-blue-600 text-white p-4 ">     
     <div class="container mx-auto flex justify-between items-center">
-        
     <div class="flex items-center">
                 <span class="font-bold text-xl">TransportExpress</span>
-    </div>
-           
+            </div>
+            <div class="hidden md:flex space-x-4">
+                <a href="#" class="hover:text-blue-200">Accueil</a>
+                <a href="#" class="hover:text-blue-200">Services</a>
+                <a href="#" class="hover:text-blue-200">À propos</a>
+                <a href="#" class="hover:text-blue-200">Contact</a>
+            </div>
+            
     </div>
     </nav>
 
