4f362ca6eab54f796dbafd7f5229cf2f535859a3
Hounaida202 <etrichih@gmail.com>
2025-04-13 13:07:54 +0000

    le div de le numero de tel avec le style sans outline

--- transportexpress/resources/views/SignUp.blade.php
+++ transportexpress/resources/views/SignUp.blade.php
@@ -24,7 +24,11 @@
                             <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
                             <input type="email" id="email" name="email" oninput="validateEmail()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
-  
+                    <!-- -------------- -->
+                        <div class="mb-6">
+                            <label for="tel" class="block text-gray-700 font-medium mb-2">Numéro de téléphone</label>
+                            <input type="tel" id="tel" name="tel" oninput="validateTel()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                        </div>
                 </form>
 
                   <div class="text-center mt-6">
