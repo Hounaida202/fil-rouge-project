b352181ad746a9f01e50f67b7407273161f09051
Hounaida202 <etrichih@gmail.com>
2025-04-13 15:17:56 +0000

    un thead pour regrouper l en tete de la table

--- transportexpress/resources/views/Dashboard.blade.php
+++ transportexpress/resources/views/Dashboard.blade.php
@@ -40,7 +40,14 @@
                         </div>
                         <div class="overflow-x-auto">
                             <table class="min-w-full divide-y divide-gray-200">
-                       
+                                <!-- ------ -->
+                                                    <thead class="bg-gray-50">
+                                                
+                                                    </thead>
+                                            
+                                                
+                                                  
+                           <!-- ------ -->                     
                             </table>
                         </div>
                     </div> 
