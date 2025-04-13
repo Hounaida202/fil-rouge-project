f4019096c0d3e1bbe114ef132b4b5bc218a7b2ee
Hounaida202 <etrichih@gmail.com>
2025-04-13 13:06:52 +0000

    le div de l email avec le style sans outline

--- transportexpress/resources/views/SignUp.blade.php
+++ transportexpress/resources/views/SignUp.blade.php
@@ -15,12 +15,15 @@
                 
                 <form action="" method="POST">
    
-                    <div class="flex  md:flex-row gap-6 mb-6">
                         <div class="w-full">
                             <label for="nom" class="text-gray-700 font-medium mb-2">Nom</label>
                             <input type="text" id="nom" name="name" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none " required>
                         </div>
-                    </div>
+                    <!-- -------------- -->
+                        <div class="mb-6">
+                            <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
+                            <input type="email" id="email" name="email" oninput="validateEmail()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                        </div>
   
                 </form>
 
