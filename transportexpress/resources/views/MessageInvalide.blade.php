79d21406991bfec64b0b71d9973648b5e7291777
Hounaida202 <etrichih@gmail.com>
2025-04-13 14:27:16 +0000

    lien pour la retour a l'accueil

--- transportexpress/resources/views/MessageInvalide.blade.php
+++ transportexpress/resources/views/MessageInvalide.blade.php
@@ -26,7 +26,9 @@
                         Votre demande d'inscription a été refusé.
                     </p>
 
-                    
+                    <a href="" class=" bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 ">
+                        Retour à l'accueil
+                    </a>
                     
                     
                     
