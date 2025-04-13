85f458fc06afde2cacdf7ac28b14702e7884f18d
Hounaida202 <etrichih@gmail.com>
2025-04-13 14:20:41 +0000

    la section e message

--- transportexpress/resources/views/MessageInvalide.blade.php
+++ transportexpress/resources/views/MessageInvalide.blade.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body class="bg-gray-100 font-sans">
-    
+    <section class="py-16">
+        
+    </section>
 </body>
 </html>
