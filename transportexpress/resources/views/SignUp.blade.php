99eeb840c191266560e36f331af204a741a61c4b
Hounaida202 <etrichih@gmail.com>
2025-04-13 13:42:54 +0000

    modifier la color de text

--- transportexpress/resources/views/SignUp.blade.php
+++ transportexpress/resources/views/SignUp.blade.php
@@ -11,42 +11,42 @@
     <section class="py-12">
         <div class="container mx-auto px-4">
             <div class="max-w-xl mx-auto bg-white rounded-lg shadow-md p-8">
-                <h1 class="text-3xl font-bold text-center mb-8">Créer votre compte</h1>
+                <h1 class="text-3xl text-[#18534F] font-bold text-center mb-8">Créer votre compte</h1>
                 
                 <form action="" method="POST">
    
                         <div class="w-full mb-6">
-                            <label for="nom" class="text-gray-700 font-medium mb-2">Nom</label>
+                            <label for="nom" class="text-[#18534F] font-medium mb-2">Nom</label>
                             <input type="text" id="nom" name="name" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none " required>
                         </div>
                     <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="email" class=" text-gray-700 font-medium mb-2">Email</label>
+                            <label for="email" class=" text-[#18534F] font-medium mb-2">Email</label>
                             <input type="email" id="email" name="email" oninput="validateEmail()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                     <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="tel" class=" text-gray-700 font-medium mb-2">Numéro de téléphone</label>
+                            <label for="tel" class="text-[#18534F] font-medium mb-2">Numéro de téléphone</label>
                             <input type="tel" id="tel" name="tel" oninput="validateTel()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                    <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="password" class=" text-gray-700 font-medium mb-2">Mot de passe</label>
+                            <label for="password" class="text-[#18534F] font-medium mb-2">Mot de passe</label>
                             <input type="password" id="password" name="password" oninput="validatePassword()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                    <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="password_confirmation" class=" text-gray-700 font-medium mb-2">Confirmer le mot de passe</label>
+                            <label for="password_confirmation" class="text-[#18534F] font-medium mb-2">Confirmer le mot de passe</label>
                             <input type="password" id="password_confirmation" name="password_confirmation" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                    <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="ville" class="block text-gray-700 font-medium mb-2">Ville</label>
+                            <label for="ville" class="block text-[#18534F] font-medium mb-2">Ville</label>
                             <input type="text" id="ville" name="ville" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                     <!-- -------------- -->
                         <div class="mb-8">
-                            <div class="block text-gray-700 font-medium mb-2">Type de compte</div>
+                            <div class="block text-[#18534F] font-medium mb-2">Type de compte</div>
                               <div class="flex flex-col md:flex-row gap-4">
                                      <!-- pour le client -->
                                 <div class=" border border-gray-300 rounded-lg p-4 cursor-pointer hover:border-blue-500 ">
