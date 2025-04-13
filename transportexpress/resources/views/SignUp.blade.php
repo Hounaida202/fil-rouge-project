946812db5a9927c316cdb3705430f8840e034cb3
Hounaida202 <etrichih@gmail.com>
2025-04-13 13:10:48 +0000

    le div de comfirmation de  mot de passe de tel avec le style sans outline

--- transportexpress/resources/views/SignUp.blade.php
+++ transportexpress/resources/views/SignUp.blade.php
@@ -21,18 +21,23 @@
                         </div>
                     <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
+                            <label for="email" class=" text-gray-700 font-medium mb-2">Email</label>
                             <input type="email" id="email" name="email" oninput="validateEmail()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                     <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="tel" class="block text-gray-700 font-medium mb-2">Numéro de téléphone</label>
+                            <label for="tel" class=" text-gray-700 font-medium mb-2">Numéro de téléphone</label>
                             <input type="tel" id="tel" name="tel" oninput="validateTel()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
                    <!-- -------------- -->
                         <div class="mb-6">
-                            <label for="password" class="block text-gray-700 font-medium mb-2">Mot de passe</label>
+                            <label for="password" class=" text-gray-700 font-medium mb-2">Mot de passe</label>
                             <input type="password" id="password" name="password" oninput="validatePassword()" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                        </div>
+                   <!-- -------------- -->
+                        <div class="mb-6">
+                            <label for="password_confirmation" class=" text-gray-700 font-medium mb-2">Confirmer le mot de passe</label>
+                            <input type="password" id="password_confirmation" name="password_confirmation" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                         </div>
 
                 </form>
