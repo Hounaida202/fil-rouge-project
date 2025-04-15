674cbbe614a558d62acff40e4641a5f19e17031a
Hounaida202 <etrichih@gmail.com>
2025-04-15 13:39:11 +0000

    le div des commentaires

--- transportexpress/resources/views/Profile.blade.php
+++ transportexpress/resources/views/Profile.blade.php
@@ -119,6 +119,9 @@
      <!-- une section pour les commentaires -->
         <div class="mt-8 bg-white rounded-lg shadow-md p-6">
         <h2 class="text-xl font-bold mb-6 text-gray-700">Commentaires (2)</h2>
+          <div class="mb-6 pb-6 border-b border-gray-200">
+
+          </div>
         </div>
         </div>
       </section>
