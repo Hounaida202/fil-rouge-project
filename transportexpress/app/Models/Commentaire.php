b58d583e350b32f8e94af483ca8ec4dbe0a45513
Hounaida202 <etrichih@gmail.com>
2025-04-15 16:15:03 +0000

    la protection des colonnes contre les attaques

--- transportexpress/app/Models/Commentaire.php
+++ transportexpress/app/Models/Commentaire.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Commentaire extends Model
 {
-   
+    use HasFactory; //Permet de générer des commentaires fictifs via des factories pour les tests ou l'initialisation de la BDD
+
+    protected $fillable = ['description', 'auteur_id', 'cible_id'];
+
+    
 }
 
