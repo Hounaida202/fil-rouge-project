58cd5ab177c3916da05227cb1fd77069c258b7d4
Hounaida202 <etrichih@gmail.com>
2025-04-15 14:13:27 +0000

    .

--- transportexpress/database/migrations/2025_04_15_135639_create_commentaires_table.php
+++ transportexpress/database/migrations/2025_04_15_135639_create_commentaires_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('description');
             $table->foreignId('id')->constrained('users');
+            $table->foreignId('id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
