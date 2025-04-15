17e9dad734d2581d091a1d02cc90e5f0b80934dd
Hounaida202 <etrichih@gmail.com>
2025-04-15 10:44:12 +0000

    le navbar

--- transportexpress/resources/views/Profile.blade.php
+++ transportexpress/resources/views/Profile.blade.php
@@ -6,6 +6,26 @@
     <title>Document</title>
 </head>
 <body class="bg-gray-100 font-sans">
-    
+<nav class="bg-[#18534F] text-white p-4 shadow-md">
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="flex items-center">
+                <span class="font-bold text-xl">TransportExpress</span>
+            </div>
+            <div class="hidden md:flex space-x-4">
+                <a href="" class="hover:text-blue-200">Accueil</a>
+                <a href="" class="hover:text-blue-200">Services</a>
+                <a href="" class="hover:text-blue-200">À propos</a>
+                <a href="" class="hover:text-blue-200">Contact</a>
+            </div>
+            <div >
+              <a href="" class="">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h6a2 2 0 012 2v1" />
+                  </svg>
+              </a>
+          </div>
+        </div>
+    </nav>
 </body>
 </html>
