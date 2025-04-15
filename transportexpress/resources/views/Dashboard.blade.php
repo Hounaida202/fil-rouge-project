6d43db6811058ad6dab6667d3e6989bd97fe0203
Hounaida202 <etrichih@gmail.com>
2025-04-15 08:59:13 +0000

    utilise la methode put pour simuler une requete put

--- transportexpress/resources/views/Dashboard.blade.php
+++ transportexpress/resources/views/Dashboard.blade.php
@@ -94,6 +94,7 @@
                                 <div class="flex space-x-1 sm:space-x-2">
                                 <form method="POST" action="{{ route('Valide', $enAttente->id) }}">
                                 @csrf
+                                @method('PUT')
                                     <button type="submit" value="Valide" class="bg-green-500 hover:bg-green-600 text-white px-2 sm:px-3 py-1 rounded text-xs sm:text-sm">
                                         Valider
                                     </button>
