d6e526680af7f75be968581d71b8a6809386c593
Hounaida202 <etrichih@gmail.com>
2025-04-15 08:53:53 +0000

    afficher la proprieté email de l'objet enattente

--- transportexpress/resources/views/Dashboard.blade.php
+++ transportexpress/resources/views/Dashboard.blade.php
@@ -78,7 +78,7 @@
                                 </div>
                             </td>
                             <td class="px-3 sm:px-6 py-4 whitespace-nowrap hidden sm:table-cell">
-                                <div class="text-sm text-gray-900"></div>
+                                <div class="text-sm text-gray-900">{{ $enAttente->email }}</div>
                             </td>
                             <td class="px-3 sm:px-6 py-4 whitespace-nowrap hidden md:table-cell">
                                 <span class="px-2 inline-flex text-xs leading-5 font-semibold">
