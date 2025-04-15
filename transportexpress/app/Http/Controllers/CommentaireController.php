83d32f72ed62921fad2cd1d8e38505ffd4a2d02a
Hounaida202 <etrichih@gmail.com>
2025-04-15 16:04:59 +0000

    la recreation du controller Commenatire

--- transportexpress/app/Http/Controllers/CommentaireController.php
+++ transportexpress/app/Http/Controllers/CommentaireController.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Commentaire;
+
+
+class CommentaireController extends Controller
+{
+
+
+    
+}
