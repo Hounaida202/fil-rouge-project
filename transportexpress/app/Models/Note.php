1a87dd320e66af1363389c6441a837de9d68c33f
Hounaida202 <etrichih@gmail.com>
2025-04-16 09:09:36 +0000

    creation d un controller note

--- transportexpress/app/Models/Note.php
+++ transportexpress/app/Models/Note.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+    protected $fillable = ['valeur', 'auteur_id', 'cible_id'];
+
+    public function auteur()
+    {
+        return $this->belongsTo(User::class, 'auteur_id');
+    }
+
+    public function cible()
+    {
+        return $this->belongsTo(User::class, 'cible_id');
+    }
 }
+
