a842b24972bd92f9f36931114368166efe267287
Hounaida202 <etrichih@gmail.com>
2025-04-22 13:15:12 +0000

    definir une classe controller UserController extends de la classe controller

---
+++ transportexpress/app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\DAOs\Interfaces\UserInterface;
+use App\Http\DAOs\Repositories\UserRepository;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+
+}
